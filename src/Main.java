3a000813bf0ac83ca6d37c62a04393e4dff51e8a
BT_1N3_32 <sample@bths.edu>
2023-09-21 10:22:58 -0400

    more updates + loop

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println()
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Welcome...");
+        System.out.print("How many people are you sharing your meal with? ");
+        int people = scan.nextInt();
+        System.out.print("How much do you want to tip? It is recommended to tip above 15 percent! ");
+        int percent = scan.nextInt();
+        System.out.print("Enter a cost + cents, type in -1 to end: ");
+        double costPerThing = scan.nextDouble();
+
+        while (costPerThing != -1 ) {
+            System.out.print("Enter another cost + cents, type in -1 to end: ");
+            costPerThing = costPerThing + scan.nextDouble();
+        }
 
     }
 }
